a6697a3ad5f1f3cebaf2d90f6e5ea1a7c6ae2d3a
Jyoti Shukla <jyoti.shukla@vcontractor.co.za>
2021-07-02 14:51:08 +0530

    change in member

--- src/main/java/com/trasportManagement/transportservice/controller/RegistrationController.java
+++ src/main/java/com/trasportManagement/transportservice/controller/RegistrationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/register")
+    @RequestMapping("/register")
 public class RegistrationController {
     @Autowired
     private RegistrationService registrationService;
